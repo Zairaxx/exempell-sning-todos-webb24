c454ac36ad8be769d1588e1c62faa37855e1e2dc
Zairaxx <brandontsegai@gmail.com>
2024-10-25 15:35:41 +0200

    added comments

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ btn.addEventListener("click", () => {
     let choice = document.querySelector("[name='color']:checked")
     console.log("Vald färg", choice.value);
 
+
+    //Hitta samtliga valda checkboxar
    let allCheckedBoxes = document.querySelectorAll("[name='brands']:checked")
-   
+   //Skriv ut samtliga valda värden 
    allCheckedBoxes.forEach(checkbox => console.log("Vald märke:", checkbox.value))
 
 })
